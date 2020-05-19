19030d8caa21b99f448366a40c2c635509cf325e
eager-class-5814 <candacehchoe@gmail.com>
2020-05-19 19:54:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,9 @@ def monopoly_with_third_tier
   base_hash = {
     :railroads => {
       :pieces => 4,
-      :names => {},
+      :names => {
+        
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
